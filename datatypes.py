7e17e98f816ba1be90f673a1f4026ccf97383bfb
Navesh <navesh.professional@gmail.com>
2024-06-30 00:33:55 +0530

    DataTypes in Python

---
+++ datatypes.py
@@ -0,0 +1,21 @@
+# Numbers
+# String
+# list
+# tuple
+# dictionary
+x="23"
+print(type(x))
+n=int(x)
+print(type(n))
+n=float(x)
+print(type(n))
+
+# Floor division
+x = 20
+y = 3
+result = x // y
+print(result)
+x = -10
+y = 3
+result = x // y
+print(result)
