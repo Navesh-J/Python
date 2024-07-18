e1421cc7002f9dbfa380a30be17676552adeabcd
Navesh <navesh.professional@gmail.com>
2024-07-18 15:47:35 +0530

    String Slicing

---
+++ strings_numbers.py
@@ -0,0 +1,22 @@
+if True:
+    print ("Answer")
+    print ("True")
+else:                                       #INDENTATION
+    print ("Answer")
+print ("False")
+#this is a comment
+
+a=10
+b=20
+del a,b #here a and b becomes undeclared
+
+str = 'Computer Science'
+print (str)         # Prints complete string
+print (str[0])      # Prints first character of the string
+print (str[2:5])    # Prints characters starting from 3rd to 5th
+print (str[2:])     # Prints string starting from 3rd character
+print (str * 2)     # Prints string two times
+print (str + " Engineering") # Prints concatenated string
+
+str="""This is a paragraph"""
+print(str)
